d8e3b697d02d170179a87de66a46130c66ccb3b6
Matt Turner <mattst88@gentoo.org>
2020-10-29 20:31:10 -0400

    wip
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 
-from glob import glob
-
 from ppsetuptools import setup
 
-data_files = [
-    ('/etc/catalyst', ['etc/catalyst.conf', 'etc/catalystrc']),
-    ('/usr/share/man/man1', ['files/catalyst.1']),
-    ('/usr/share/man/man5',
-     ['files/catalyst-config.5', 'files/catalyst-spec.5']
-    ),
-    ('share/catalyst/arch', glob('arch/*')),
-    ('share/catalyst/livecd', glob('livecd/**/*', recursive=True)),
-    ('share/catalyst/targets', glob('targets/**', recursive=True)),
-]
-
-setup(data_files=data_files)
+setup(
+    packages=[
+        'catalyst', 'catalyst/base', 'catalyst/targets'
+    ],
+)
